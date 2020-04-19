83054b9839e6b37024b015b149fafc8446ed8144
kengo0629 <63942635+kengo0629@users.noreply.github.com>
2020-04-19 17:59:59 +0900

    initial commit

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,12 @@
+class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  module ChatSpace
+    class Aplication < Rails::Application
+      config.generators do |g|
+        g.stylesheets false
+        g.javascripts false
+        g.helper false
+        g.test_framework false
+      end
+  end
+end
